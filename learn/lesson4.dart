3f461e7dfbe659057f3ab660c1099483d7fa2d7b
was <zxt_zel_npl@163.com>
2018-02-07 23:12:17 +0800

    initial commit

--- learn/lesson4.dart
+++ learn/lesson4.dart
@@ -1,7 +1,15 @@
 void main(){
 
-
   assert(sayHello1 is Function);
+
+  var x = makeSubstract(5);
+  print(x(2));
+
+  var callbacks = [];
+  for(var i=0;i<3;i++){
+    callbacks.add(()=>print('Save $i'));
+  }
+  callbacks.forEach((c)=>c());
 }
 
 String sayHello1(String name)
@@ -16,4 +24,9 @@ sayHello2(name){
 sayHello3(name) => 'Hello $name';
 
 var sayHello4 = (name) => 'Hello $name';
+
+Function makeSubstract(num n)
+{
+  return (num i)=>n-i;
+}
 
